b749d49e0786e10f8bd179115cf584e4a0c75a1f
Aaron Greider <40152187+aarongreider@users.noreply.github.com>
2024-07-08 14:44:27 -0400

    restyle

--- src/components/toolbar/Toolbar.tsx
+++ src/components/toolbar/Toolbar.tsx
@@ -46,49 +46,41 @@ function Toolbar({ sections, categories, printRef, groupSelect, openList }: prop
     return <>
         <div id='toolbar'>
             {/* SHOPPING LIST BUTTONS */}
-            <div tabIndex={0} className="header" onClick={openList} style={{ order: 3 }}>
+            <div tabIndex={0} className="header toolbarButton" onClick={openList} style={{ order: 3 }}>
                 <span className="material-symbols-outlined listIcon">receipt_long</span>
                 <h1>View Shopping List</h1>
                 <span id="listCounter" className="button">{shoppingList.length}</span>
             </div>
 
-            <span tabIndex={0} className="material-symbols-outlined print" onClick={() => handlePrint(printRef)} style={{ order: 4 }}>print</span>
+            <span tabIndex={0} className="material-symbols-outlined print toolbarButton" onClick={() => handlePrint(printRef)} style={{ order: 4 }}>print</span>
 
             {/* SECTION DROPDOWN */}
             <label htmlFor="sectionDropdown">Filter Section</label>
 
-            <div className="copycat" ref={sectionCopycat}>
+            <div className="copycat toolbarButton" ref={sectionCopycat}>
                 <p id="copycatSection">{activeSection.toLocaleLowerCase()}</p>
             </div>
 
-            <select id="sectionDropdown" ref={sectionDropdown} className="filterButton" style={{ width: `${sectionWidth}px` }} data-chosen-disable="true"
-                onChange={(event) => { handleChange(event, 'section') }}>
-
-                <option value="All Sections">All Sections</option>
-                {sections.map((section, index) => {
-                    return <option key={index} value={section}>{section.toLowerCase()}</option>
-                })}
-            </select>
+            <div className="filter">
+                <select id="sectionDropdown" className="toolbarButton" ref={sectionDropdown} data-chosen-disable="true" style={{ width: `${sectionWidth && sectionWidth + 20}px` }}
+                    onChange={(event) => { handleChange(event, 'section') }}>
+                    <option value="All Sections">All Sections</option>
+                    {sections.map((section, index) => {
+                        return <option key={index} value={section}>{section.toLowerCase()}</option>
+                    })}
+                </select>
+                <span className="material-symbols-outlined">keyboard_arrow_up</span>
+            </div>
 
             {/* CATEGORY DROPDOWN */}
             <label htmlFor="categoryDropdown">Filter Category</label>
 
-            <div className="copycat" >
-                <p ref={categoryCopycat} id="copycatCategory">{activeCategory.toLocaleLowerCase()}</p>
+            <div className="copycat toolbarButton" ref={categoryCopycat}>
+                <p id="copycatCategory">{activeCategory.toLocaleLowerCase()}</p>
             </div>
 
-            <div className="filterButton" style={{ width: `${categoryWidth && categoryWidth + 30}px` }}
-                onClick={(event) => {
-                    console.log(event);
-                    const target = event.target as HTMLElement;
-                    const select = target.querySelector('select');
-                    if (select) {
-                        console.log(select)
-                        select.focus()
-                        select.click()
-                    }
-                }}>
-                <select id="categoryDropdown" ref={categoryDropdown} data-chosen-disable="true" style={{ width: `${categoryWidth && categoryWidth}px` }}
+            <div className="filter">
+                <select id="categoryDropdown" className="toolbarButton" ref={categoryDropdown} data-chosen-disable="true" style={{ width: `${categoryWidth && categoryWidth + 20}px` }}
                     onChange={(event) => { handleChange(event, 'category') }}>
                     <option value="All Categories">All Categories</option>
                     {categories.map((category, index) => {
